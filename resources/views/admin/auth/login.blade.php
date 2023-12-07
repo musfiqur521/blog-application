2644d4e8b98b23d85ba7696a5d4553f9c92a330f
musfiqur521 <musfiqur521@gmail.com>
2023-12-07 15:27:11 +0600

    complete_adminDashboard

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,4 +1,12 @@
 <x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+            Admin Login
+        </x-slot>
+
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
     Admin Login
@@ -45,4 +53,5 @@
             </x-primary-button>
         </div>
     </form>
+</x-auth-card>
 </x-guest-layout>
--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -67,7 +67,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('admin.dashboard')">
+            <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                 {{ __('Admin Dashboard') }}
             </x-responsive-nav-link>
         </div>
@@ -75,8 +75,8 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <div class="font-medium text-base text-gray-800">{{ Auth::guard('admin')->user()->name }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::guard('admin')->user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
